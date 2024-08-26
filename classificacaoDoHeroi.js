497f543ea1b6ba775e8fd64877a9e4b41bb82a23
leonardo.walkowski <leowalkowski@gmail.com>
2024-08-26 14:25:46 -0300

    classificacaoDoHeroi-parte2-commit

--- classificacaoDoHeroi.js
+++ classificacaoDoHeroi.js
@@ -35,25 +35,25 @@ let nome = "Super Tupiniquim"
 // let xp = 7000
 // let xp = 8000
 // let xp = 1000
-// let xp = 9000
+ let xp = 9000
 // let xp = 10000
- let xp = 11000
+ //let xp = 11000
 
 let nivelDoHeroi;
 
 if(xp < 1000) {
     nivelDoHeroi = "ferro"
-} else if (xp <= 2000) {
+} else if (xp >= 1001 && xp <= 2000) {
     nivelDoHeroi = "Bronze" 
-} else if (xp <= 5000) {
+} else if (xp >= 2001 && xp <= 5000) {
     nivelDoHeroi = "Prata"
-} else if (xp <= 7000) {
+} else if (xp >= 5001 && xp <= 7000) {
     nivelDoHeroi = "Ouro"
-} else if (xp <= 8000) {
-    nivelDoHeroi
-} else if (xp <= 9000) {
+} else if (xp >= 7001 && xp <= 8000) {
+    nivelDoHeroi = "Platina"
+} else if (xp >= 8001 && xp <= 9000) {
     nivelDoHeroi = "Ascendente"
-} else if (xp <= 10000) {
+} else if (xp >= 9001 && xp <= 10000) {
     nivelDoHeroi = "Imortal"
 } else {
     nivelDoHeroi = "Radiante"
